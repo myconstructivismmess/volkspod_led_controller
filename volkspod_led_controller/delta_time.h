64b0e3498006ec881195c8c0a83cbfbe564bc89e
Aubrey ROUET <aubrey.rouet@gmail.com>
2025-02-12 11:37:13 +0100

    Add global static deltaTime calculation function

---
+++ volkspod_led_controller/delta_time.h
@@ -0,0 +1,41 @@
+// +------------------------------------------------------------------------------+
+// | filename: delta_time.h                                                       |
+// | filepath: volkspod_led_controller/delta_time.h                               |
+// | project name: volkspod_led_controller                                        |
+// | authors: Aubrey ROUET <aubrey.rouet@gmail.com>                               |
+// | license: MIT                                                                 |
+// |                                                                              |
+// | Copyright (c) 2025 Aubrey ROUET                                              |
+// |                                                                              |
+// | Permission is hereby granted, free of charge, to any person obtaining a copy |
+// | of this software and associated documentation files (the "Software"), to     |
+// | deal in the Software without restriction, including without limitation the   |
+// | rights to use, copy, modify, merge, publish, distribute, sublicense, and/or  |
+// | sell copies of the Software, and to permit persons to whom the Software is   |
+// | furnished to do so, subject to the following conditions:                     |
+// |                                                                              |
+// | The above copyright notice and this permission notice shall be included in   |
+// | all copies or substantial portions of the Software.                          |
+// |                                                                              |
+// | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   |
+// | IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     |
+// | FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  |
+// | AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       |
+// | LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      |
+// | FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS |
+// | IN THE SOFTWARE.                                                             |
+// +------------------------------------------------------------------------------+
+
+// ----- Delta Time Class ---------------------------------------------------------
+
+#ifndef _DELTA_TIME_CLASS_H_
+#define _DELTA_TIME_CLASS_H_
+
+class DeltaTime {
+    public:
+        static const unsigned long calculate(const unsigned long currentTimeMs, const unsigned long lastTimeMs);
+};
+
+#endif
+
+// --------------------------------------------------------------------------------
